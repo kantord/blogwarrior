9a12e3b341ae04646e401db4e2e86d0b5fb217b5
Dániel Kántor <git@daniel-kantor.com>
2026-03-07 18:30:08 +0000

    perf: improve blog sync performance (#76)

--- src/utils/http.rs
+++ src/utils/http.rs
@@ -1,7 +1,8 @@
 pub(crate) fn http_client() -> anyhow::Result<reqwest::blocking::Client> {
     reqwest::blocking::Client::builder()
-        .user_agent(format!("blogtato/{}", env!("CARGO_PKG_VERSION")))
-        .timeout(std::time::Duration::from_secs(30))
+        .user_agent("Mozilla/5.0 (compatible; blogtato RSS reader)")
+        .timeout(std::time::Duration::from_secs(10))
+        .pool_max_idle_per_host(0)
         .build()
         .map_err(|e| anyhow::anyhow!("failed to build HTTP client: {}", e))
 }
