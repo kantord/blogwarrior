c06a87c4c95c4ba805486206dc951a8cbe7f0ef9
Daniel Kantor <git@daniel-kantor.com>
2026-03-31 09:34:22 +0200

    .

--- spud/app/bin/view.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-link=$(jq -r '.link')
-blog .all export 2>/dev/null | jq -c --arg link "$link" 'select(.link == $link) | {title: .title, link: .link, feed_title: .feed.title, date: (.date // "" | if . != "" then .[:10] else "" end)}'
